47bfe16c3002119dc1dfa0d684d9a2125ef106ac
GitHub Action <action@github.com>
2025-06-02 14:27:04 +0000

    Initialize template repository proper
    Set package name to com.github.nolifejavadeveloper.yogurtaddons
    Set project name to YogurtAddons
    Set modid to yogurtaddons

--- src/main/java/com/github/nolifejavadeveloper/yogurtaddons/ExampleMod.java
+++ src/main/java/com/github/nolifejavadeveloper/yogurtaddons/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.nolifejavadeveloper.yogurtaddons;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "yogurtaddons", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
