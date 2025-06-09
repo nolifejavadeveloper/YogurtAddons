c37bf2aab395554597e234fc6868bc1af6daee7a
ethan <nope@gg.net>
2025-06-08 23:21:21 -0400

    changed package

--- src/main/java/net/ethann/yogurtaddons/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/net/ethann/yogurtaddons/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.github.nolifejavadeveloper.yogurtaddons.init;
+package net.ethann.yogurtaddons.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
