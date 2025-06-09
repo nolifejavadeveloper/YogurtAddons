c323a7f354ea9e2599555532a867c3a57ab6b76c
ethan <nope@gg.net>
2025-06-08 23:22:08 -0400

    added helper vec2 and vec3 class

---
+++ src/main/java/net/ethann/yogurtaddons/util/Vec2.java
@@ -0,0 +1,15 @@
+package net.ethann.yogurtaddons.util;
+
+public class Vec2 {
+    public int x = 0;
+    public int y = 0;
+
+    public Vec2() {
+
+    }
+
+    public Vec2(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/main/java/net/ethann/yogurtaddons/util/Vec3.java
@@ -0,0 +1,17 @@
+package net.ethann.yogurtaddons.util;
+
+public class Vec3 {
+    public int x = 0;
+    public int y = 0;
+    public int z = 0;
+
+    public Vec3() {
+
+    }
+
+    public Vec3(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
