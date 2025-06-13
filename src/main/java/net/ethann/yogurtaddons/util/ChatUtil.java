f56182e9959ecb319e2f45ed8fe03a22c16ef3f5
what <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-06-13 14:26:30 -0400

    added chatutil for logging in game

--- src/main/java/net/ethann/yogurtaddons/util/ChatUtil.java
+++ src/main/java/net/ethann/yogurtaddons/util/ChatUtil.java
@@ -1,9 +1,25 @@
 package net.ethann.yogurtaddons.util;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.ChatComponentText;
 
 public class ChatUtil {
+    private static final Minecraft mc = Minecraft.getMinecraft();
+    private static final String PREFIX = "§a§lYo§e§lgurt§r§f";
+
     public static void sendCommand(String s) {
-        Minecraft.getMinecraft().thePlayer.sendChatMessage("/" + s);
+        mc.thePlayer.sendChatMessage("/" + s);
+    }
+
+    public static void log(String s) {
+        mc.thePlayer.addChatComponentMessage(new ChatComponentText(PREFIX + " > " + s));
+    }
+
+    public static void warn(String s) {
+        mc.thePlayer.addChatComponentMessage(new ChatComponentText(PREFIX + " §6[WARN]§r > " + s));
+    }
+
+    public static void error(String s) {
+        mc.thePlayer.addChatComponentMessage(new ChatComponentText(PREFIX + " §c[Error]§r > " + s));
     }
 }
