f41e10b9cf4e4762b7ded292370de5f27e10bc31
ethan <nope@gg.net>
2025-06-13 23:45:34 -0400

    fixed rightClick method

--- src/main/java/net/ethann/yogurtaddons/util/ControlUtil.java
+++ src/main/java/net/ethann/yogurtaddons/util/ControlUtil.java
@@ -1,12 +1,23 @@
 package net.ethann.yogurtaddons.util;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class ControlUtil {
-    public static void interact() {
-        EntityPlayerSP player = Minecraft.getMinecraft().thePlayer;
-        player.interactFirst(player);
+    private static final int REACH = 3;
+
+    public static void rightClick() {
+        Minecraft mc = Minecraft.getMinecraft();
+        Method rightClickMethod;
+        try {
+            rightClickMethod = Minecraft.class.getDeclaredMethod("rightClickMouse");
+            rightClickMethod.setAccessible(true);
+            rightClickMethod.invoke(mc);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
