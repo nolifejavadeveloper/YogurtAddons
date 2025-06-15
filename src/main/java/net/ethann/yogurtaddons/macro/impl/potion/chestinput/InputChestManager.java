c6a9fc4a3e6e7ff620c238adb833de9db3f7f266
ethan <nope@gg.net>
2025-06-15 12:22:34 -0400

    added loading and saving for InputChestManager

--- src/main/java/net/ethann/yogurtaddons/macro/impl/potion/chestinput/InputChestManager.java
+++ src/main/java/net/ethann/yogurtaddons/macro/impl/potion/chestinput/InputChestManager.java
@@ -1,12 +1,27 @@
 package net.ethann.yogurtaddons.macro.impl.potion.chestinput;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import net.ethann.yogurtaddons.util.FileUtil;
 import net.minecraft.util.BlockPos;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
+import net.minecraftforge.fml.common.Loader;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 public class InputChestManager {
+    private static final String FILE_NAME = "chestInputs.json";
     private List<BlockPos> chests = new ArrayList<>();
+
+    public InputChestManager() {
+        load();
+    }
 
     public void addChest(BlockPos p) {
         chests.add(p);
@@ -18,5 +33,36 @@ public class InputChestManager {
 
     public List<BlockPos> getAll() {
         return chests;
+    }
+
+    private void load() {
+        File file = FileUtil.getConfigFileFromModPath(FILE_NAME);
+        String content;
+        try {
+            content = FileUtil.readFile(file);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read " + FILE_NAME + ": " + e.getMessage());
+        }
+
+        Type type = new TypeToken<List<BlockPos>>() {}.getType();
+        Gson json = new Gson();
+
+        chests = json.fromJson(content, type);
+    }
+
+    private void save() {
+        File file = FileUtil.getConfigFileFromModPath(FILE_NAME);
+        Gson json = new GsonBuilder().setPrettyPrinting().create();
+
+        String content = json.toJson(chests);
+        try {
+            File parent = file.getParentFile();
+            if (!parent.exists()) {
+                parent.mkdirs();
+            }
+            FileUtil.writeFile(file, content);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to write " + FILE_NAME + ": " + e.getMessage());
+        }
     }
 }
