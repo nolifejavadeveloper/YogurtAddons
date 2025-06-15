f813f25770da063fcfbe9bd15f2873dfef0f8f94
ethan <nope@gg.net>
2025-06-15 11:18:52 -0400

    added read and write util

--- src/main/java/net/ethann/yogurtaddons/util/FileUtil.java
+++ src/main/java/net/ethann/yogurtaddons/util/FileUtil.java
@@ -4,6 +4,9 @@ import net.ethann.yogurtaddons.YogurtAddons;
 import net.minecraftforge.fml.common.Loader;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 public class FileUtil {
     public static File getConfigFileFromModPath(String name) {
@@ -12,5 +15,13 @@ public class FileUtil {
 
     public static File getConfigFromForgePath(String name) {
         return new File(Loader.instance().getConfigDir(),  name);
+    }
+
+    public static String readFile(File file) throws IOException {
+        return new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+    }
+
+    public static void writeFile(File file, String content) throws IOException {
+        Files.write(file.toPath(), content.getBytes(StandardCharsets.UTF_8));
     }
 }
